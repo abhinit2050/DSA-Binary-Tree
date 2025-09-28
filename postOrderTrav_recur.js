f96cccc4c5233fb51556a1bc0f678c6465db8233
Abhinit Rai <abhinit1990@gmail.com>
2025-09-28 20:47:00 +0530

    post order tree traversal using recursion

---
+++ postOrderTrav_recur.js
@@ -0,0 +1,16 @@
+var postorderTraversal = function(root) {
+    
+    let ans=[];
+
+    function traversal(curr){
+        if(!curr) return;
+
+        traversal(curr.left);
+        traversal(curr.right);
+        ans.push(curr.val)
+    }
+    traversal(root);
+
+    return ans;
+
+};
