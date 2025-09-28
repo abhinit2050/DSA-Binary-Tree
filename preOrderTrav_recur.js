9cb8d323f5698aa16869a5113b92d2220de3bde7
Abhinit Rai <abhinit1990@gmail.com>
2025-09-28 20:24:44 +0530

    preorder traversal using recursion

---
+++ preOrderTrav_recur.js
@@ -0,0 +1,17 @@
+var preorderTraversal = function(root) {
+    let ans=[];
+
+    function traversal(curr){
+    if(!curr) return [];
+        ans.push(curr.val);
+        traversal(curr.left);
+        traversal(curr.right)
+        
+}
+
+traversal(root);
+
+return ans;
+
+};
+
