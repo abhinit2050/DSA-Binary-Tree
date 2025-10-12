b4ea94eaf9ac28df15af7b68af609ae6fd78b951
Abhinit Rai <abhinit1990@gmail.com>
2025-10-12 12:31:08 +0530

    balanced binary tree

---
+++ balancedBinaryTree.js
@@ -0,0 +1,23 @@
+var isBalanced = function(root) {
+
+    let ans = true;
+
+    var calculateHeight= function(curr){
+
+        if(!curr) return 0;
+
+        let leftHeight = calculateHeight(curr.left);
+        let rightHeight = calculateHeight(curr.right);
+
+        if(Math.abs(leftHeight-rightHeight)>1){
+            ans = ans && false
+        }
+
+        return 1 + Math.max(leftHeight, rightHeight)
+    }
+
+    calculateHeight(root);
+
+    return ans;
+    
+};
